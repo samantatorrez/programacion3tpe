77fbfd1178a7f45fbdf616ccf1425639d0bcd7ba
Samanta Torrez <dsamantatorrez@gmail.com>
2018-04-30 09:19:30 -0300

    Read csv

--- src/programacion3tpe/CSVConverterService.java
+++ src/programacion3tpe/CSVConverterService.java
@@ -1,5 +1,37 @@
 package programacion3tpe;
 
-public class CSVConverterService {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-}
+public class CSVConverterService {
+	public static final String SEPARATOR=",";
+	public static final String SEPARATOR_GENEROS=" ";
+	public static List<Libro> readCSV() throws IOException{
+		List<Libro> libros= new ArrayList<Libro>();
+		  
+			   String csvFile = "datasets/dataset1.csv";
+		        String line = "";
+
+		        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+		        	br.readLine();
+		            while ((line = br.readLine()) != null) {
+		                String [] fields = line.split(SEPARATOR);
+				         String [] generos = fields[3].split(SEPARATOR_GENEROS);
+				         libros.add(new Libro(fields[0],fields[1],Integer.parseInt(fields[2]),Arrays.asList(generos)));      
+				         line = br.readLine();
+		            }
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        
+		   } 
+	    return libros;
+	}
+	public static void writeCSV(List<Libro> libros) {
+		
+	}
+} 
+   
--- src/programacion3tpe/main.java
+++ src/programacion3tpe/main.java
@@ -1,11 +1,17 @@
 package programacion3tpe;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class main {
 
 	public static void main(String[] args) {
-		
+		try {
+			CSVConverterService.readCSV();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
